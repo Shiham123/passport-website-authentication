0660087bc4232fdbcbd9041cec091dc4a6e6cc07
Shiham123 <s36651786@gmail.com>
2023-08-14 08:59:43 +0600

    session create and save with mongo-connect and also passport and ie

--- app.js
+++ app.js
@@ -2,21 +2,48 @@ const express = require('express');
 const cors = require('cors');
 const ejs = require('ejs');
 const app = express();
+
 require('./config/database');
 const user = require('./Model/user.model');
+
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+
+const passport = require('passport');
+const session = require('express-session');
+const mongoStore = require('connect-mongo');
+
+require('dotenv').config();
+const dbUrl = process.env.dbUrl;
+
+// ! other import section is upper
 
 app.set('view engine', 'ejs');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set('trust proxy', 1); // trust first proxy
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: mongoStore.create({
+      mongoUrl: dbUrl,
+      collectionName: 'sessions',
+    }),
+    // cookie: { secure: true },
+  })
+);
+
+// ! some method declare here form import method
+
 app.get('/', (request, response) => {
   response.status(200).render('index');
 });
 
-// --------------------------------------------------
+// ! get method for home route
 
 app.get('/register', (request, response) => {
   response.status(200).render('register');
@@ -45,7 +72,7 @@ app.post('/register', async (request, response) => {
   }
 });
 
-// --------------------------------------------------
+// ! get method and post method for register route
 
 app.get('/login', (request, response) => {
   response.status(200).render('login');
